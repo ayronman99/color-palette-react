b4897c63d75f5253a8719bbce9c06272092c0c10
ayronman99 <itechman32@gmail.com>
2022-09-02 18:27:32 +0800

    Updates

---
+++ precache-manifest.146d6fcce45cab972847bca42cc39817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3983ec535dfbcc175ab8",
+    "url": "/color-palette-react/static/css/main.1d9529ef.chunk.css"
+  },
+  {
+    "revision": "3983ec535dfbcc175ab8",
+    "url": "/color-palette-react/static/js/main.3e0d90ed.chunk.js"
+  },
+  {
+    "revision": "bb1a71bca833acaa2751",
+    "url": "/color-palette-react/static/js/runtime~main.1996a8c8.js"
+  },
+  {
+    "revision": "6475572f2437e27accd5",
+    "url": "/color-palette-react/static/css/2.7ca56b9b.chunk.css"
+  },
+  {
+    "revision": "6475572f2437e27accd5",
+    "url": "/color-palette-react/static/js/2.e6c8ef42.chunk.js"
+  },
+  {
+    "revision": "b3595806f281e9ab3a3645ab6e6a0463",
+    "url": "/color-palette-react/static/media/bg.b3595806.svg"
+  },
+  {
+    "revision": "c7881269eae766a67c9ed9a2eb757e88",
+    "url": "/color-palette-react/index.html"
+  }
+];
